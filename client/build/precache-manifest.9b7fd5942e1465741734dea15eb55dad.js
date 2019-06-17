3128c9cfd780084dfa0546f000d56331d296f76f
Shy <shyloh.elder@gmail.com>
2019-06-17 19:48:41 -0400

    trying again

---
+++ client/build/precache-manifest.9b7fd5942e1465741734dea15eb55dad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d3fd00e75b2099afd5239e22055b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c134947dddb336303b57",
+    "url": "/static/css/main.f388b48f.chunk.css"
+  },
+  {
+    "revision": "f3481347db43915d48fa",
+    "url": "/static/js/2.3b69e2c8.chunk.js"
+  },
+  {
+    "revision": "c134947dddb336303b57",
+    "url": "/static/js/main.eeaff895.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
